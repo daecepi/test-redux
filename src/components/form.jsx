499e4b8ff0d9d004b843af95537e3d51c6b3d81e
Jobsity <jobsity>
2019-10-21 17:56:45 -0500

    Two dummy components created

---
+++ src/components/form.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+import Formik from 'formik';
+
+class FormElement extends Component {
+    state = {   }
+
+    onSubmit = (e) => {
+        e.preventDefault();
+
+
+    }
+
+    render() { 
+        return ( 
+            <Formik onSubmit={this.onSubmit}> 
+
+            </Formik>
+         );
+    }
+}
+ 
+export default FormElement;
---
+++ src/components/posts.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Pos extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <>
+                <h1>Hello</h1>
+                <p>COmponent oine</p>
+            </>
+         );
+    }
+}
+ 
+export default Pos;
